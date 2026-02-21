15c57f7d9472ad642051bab0574119238b2e1c85
DianaSadova <dsadova060@gmail.com>
2026-02-21 18:26:13 +0300

    Lab1

---
+++ labs/lab01/setup_project.jl
@@ -0,0 +1,11 @@
+##!/usr/bin/env julia
+## setup_project.jl
+using Pkg
+Pkg.add("DrWatson")
+using DrWatson
+
+## Создание проекта
+project_name = "project"
+initialize_project(project_name; authors="Diana S.", git=false)
+println("✅ Проект создан: ", project_name)
+println("� Перейдите в директорию: cd ", project_name)
